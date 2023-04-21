050e0ceb63942c5aad8a52933896add31c2202d3
Alessandro <as-web@hotmail.com>
2023-04-21 10:33:49 +0100

    site map updated

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -9,8 +9,8 @@ export default function handler(req: any, res: any) {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
       <url>
-        <loc>http://www.example.com/foo.html</loc>
-        <lastmod>2021-01-01</lastmod>
+        <loc>https://www.antoninospizza.co.uk/</loc>
+        <lastmod>2023-05-21</lastmod>
       </url>
       </urlset>`;
 
