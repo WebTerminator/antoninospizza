bcf4c0602821ad6e554d6481b6077ffdb87e0b34
Alessandro <as-web@hotmail.com>
2023-04-21 10:36:41 +0100

    site map updated

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -8,10 +8,22 @@ export default function handler(req: any, res: any) {
   // generate sitemap here
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-      <url>
-        <loc>https://www.antoninospizza.co.uk/</loc>
-        <lastmod>2023-05-21</lastmod>
-      </url>
+        <url>
+          <loc>https://www.antoninospizza.co.uk/</loc>
+          <lastmod>2023-05-21</lastmod>
+        </url>
+        <url>
+          <loc>https://www.antoninospizza.co.uk/about</loc>
+          <lastmod>2023-05-21</lastmod>
+        </url>
+        <url>
+          <loc>https://www.antoninospizza.co.uk/menu</loc>
+          <lastmod>2023-05-21</lastmod>
+        </url>
+        <url>
+          <loc>https://www.antoninospizza.co.uk/contact</loc>
+          <lastmod>2023-05-21</lastmod>
+        </url>
       </urlset>`;
 
   res.end(xml);
